cd23cea30eb10d3cfedc6ff0c70abac48bf3051e
JavidLalayev <cavidleleyev9a12014@gmail.com>
2020-12-20 18:28:33 +0400

    first commit

---
+++ application/views/admin/includes/scripts.php
@@ -0,0 +1,13 @@
+
+
+<!-- js -->
+<script src="<?php echo base_url("public/admin/")?>vendors/scripts/core.js"></script>
+<script src="<?php echo base_url("public/admin/")?>vendors/scripts/script.min.js"></script>
+<script src="<?php echo base_url("public/admin/")?>vendors/scripts/process.js"></script>
+<script src="<?php echo base_url("public/admin/")?>vendors/scripts/layout-settings.js"></script>
+
+
+
+
+</body>
+</html>
